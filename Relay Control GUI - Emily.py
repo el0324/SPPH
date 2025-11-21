f28341214b086f124f24bfc1e26528da1fe76adf
Emily Lee <0324emily@gmail.com>
2025-11-21 12:10:45 -0800

    [got basic logging function and restored background img]

--- Relay Control GUI - Emily.py
+++ Relay Control GUI - Emily.py
@@ -6,26 +6,40 @@
 #Import
 import sys
 import threading
-from pyfirmata2 import ArduinoMega 
+import csv
+from pyfirmata2 import ArduinoMega
 import tkinter as tk
 from tkinter import * 
 import time
 from time import sleep
 from datetime import datetime
 
+#================================= Constants =================================#
+BOARD         = ArduinoMega('COM5') 
+WIN           = tk.Tk()
+MY_CANVAS     = tk.Canvas(WIN, width = 1280, height= 660)
+TITLE_FONT    = ('Arial', 40, 'bold')
+INPUT_FONT    = ('Arial', 50)
+REGULAR_FONT  = ('Arial', 30)
+STOP_FONT     = ('Arial', 40, 'bold')
+FIELDNAMES    = ['RUN', 'RELAY', 'START TIME', 'END TIME']
+DATE          = datetime.date(datetime.today())
+
 #============================== Global Variables =============================#
 # test variables
-PinList     = []
-Relay_Names = []
-run_test    = False
-delay       = 0.0
-relay_open  = "Off"
-start_time  = 0.0
-elapsed     = 0.0
+PinList           = []
+Relay_Names       = []
+run_test          = False
+delay             = 0.0
+relay_open        = "Off"
+start_time        = 0.0
+local_start_time  = 0.0
+end_time          = 0.0
+elapsed           = 0.0
+selected_run      = ""
 
 # tkinter variables
-win                     = tk.Tk()
-my_canvas               = Canvas()
+background              = tk.PhotoImage(file="./birchbg.png")
 button_start            = Button()
 button_manual           = Button()
 manual_options          = list()
@@ -34,12 +48,7 @@ relay_time              = Entry()
 selected_relay_text     = Canvas()
 completion_text         = Canvas()
 
-#================================= Constants =================================#
-BOARD         = ArduinoMega('/dev/ttyACM0') 
-TITLE_FONT    = ('Arial', 40, 'bold')
-INPUT_FONT    = ('Arial', 50)
-REGULAR_FONT  = ('Arial', 30)
-STOP_FONT     = ('Arial', 40, 'bold')
+
 
 #================================ Functions ==================================#
 def set_relay_names():
@@ -60,12 +69,15 @@ def set_relay_names():
 def default_state():
   """ Sets the default state. Turn everyone off but the first relay, and set 
   the initial logic for some global variables. """  
-  global my_canvas 
   global run_test
   global relay_open
   global completion_text
   global selected_relay_text
   global elapsed
+  global start_time
+  global local_start_time
+  global end_time
+  global selected_run
 
   # turn all relays off
   for i in range(26,42):
@@ -74,16 +86,40 @@ def default_state():
   # keep the first relay on
   BOARD.digital[PinList[0]].write(1)
 
+  # reset completion and selected relay
   run_test = False
   relay_open = "Off"
   elapsed = 0.0
-
-  # update completion and LED selected text
-  completion_text = my_canvas.create_text(50, 495, 
-                            text="Run Percent Complete: " + f"{elapsed:.0f}" + "%", 
-                            font=REGULAR_FONT, anchor = 'nw')
-  selected_relay_text = my_canvas.create_text(50, 550, text="LED select is " + str(relay_open), 
+  completion_text = MY_CANVAS.create_text(50, 495, 
+                            text="Run Percent Complete: 0%", font=REGULAR_FONT, 
+                            anchor = 'nw')
+  selected_relay_text = MY_CANVAS.create_text(50, 550, text="LED select is Off", 
                                             font=REGULAR_FONT, anchor = 'nw')
+  
+  # reset start and end time and selecte run
+  start_time = 0.0
+  end_time = 0.0
+  selected_run = ""
+  
+def logging(run: str, relay: str, start, end):
+  """ Logs data to a .csv file with the name of the file being the date it was
+  created (ex. 11-14-2025.csv). The log data include the type of run (automated
+  or manual), when the run started, and when the run was completed/ended """
+
+  # convert timestamps into local time
+  start = time.strftime("%H:%M:%S", start)
+  end = time.strftime("%H:%M:%S", end)
+
+  # format data
+  data = {'RUN': f'{run}', 
+          'RELAY': f'{relay}', 
+          'START TIME': f'{start}', 
+          'END TIME': f'{end}'}
+
+  if run != "":
+    with open(f'{DATE}.csv', 'a', newline='') as csvfile:
+      writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+      writer.writerow(data)
 
 #=========================== Automated Run Code: =============================#
 def button_stop_command():
@@ -94,16 +130,29 @@ def button_stop_command():
   global button_start
   global button_manual
   global relay_open
+  global local_start_time
+  global end_time
+  global selected_run
+  global selected_relay
+
+  # get end time
+  end_time = time.localtime()
+
+  # log data
+  if selected_run == "Automated":
+    selected_relay = "x"
+  logging(selected_run, selected_relay, local_start_time, end_time)
 
   # reset GUI settings
   run_test = False
   relay_open = "Off"
   button_start['state'] = tk.NORMAL
   button_manual['state'] = tk.NORMAL
+  button_exit['state'] = tk.NORMAL
 
   # set relays to default state
-  my_canvas.delete(completion_text)
-  my_canvas.delete(selected_relay_text)
+  MY_CANVAS.delete(completion_text)
+  MY_CANVAS.delete(selected_relay_text)
   default_state()
 
 def button_start_command():
@@ -113,25 +162,35 @@ def button_start_command():
   global relay_time
   global relay_open
   global start_time
+  global local_start_time
   global button_start
   global button_manual
+  global button_exit
+  global selected_run
 
+  
+  # if transitioning from manual to automated code, intial to default state
+  if selected_run == "Manual":
+    MY_CANVAS.delete(completion_text)
+    MY_CANVAS.delete(selected_relay_text)
+    default_state()
+  
   # get the delay and start time
   delay = float(relay_time.get())
   start_time = time.time()
+  local_start_time = time.localtime()
+  selected_run = "Automated"
 
-  # if transitioning from manual to automated code, intial to default state
-  my_canvas.delete(completion_text)
-  my_canvas.delete(selected_relay_text)
-  default_state()
+  # prepare to start test
   run_test = True
   current_relay = 1
-  
+
   while current_relay <= (len(PinList)-1) and run_test:
     # Disable start buttons once this loop starts so nothing gets pressed twice
     # and open multiple samplers
     button_start['state'] = tk.DISABLED 
     button_manual['state'] = tk.DISABLED
+    button_exit['state'] = tk.DISABLED
 
     # open one relay, wait for delay amount of time, then close relay
     relay_open = Relay_Names[current_relay]
@@ -141,12 +200,14 @@ def button_start_command():
     current_relay += 1
     BOARD.digital[PinList[current_relay-1]].write(1)
 
-    # if all relays have been tested, turn all relays expect the first
-    # one off
-    if current_relay == (len(PinList)):
-      my_canvas.delete(completion_text)
-      my_canvas.delete(selected_relay_text)
-      default_state()
+  # get end time and log data
+  end_time = time.localtime()
+  logging(selected_run, 'x', local_start_time, end_time)
+
+  # return to default state once run is complete
+  MY_CANVAS.delete(completion_text)
+  MY_CANVAS.delete(selected_relay_text)
+  default_state()
 
   # Re-enable buttons once test is done
   button_start['state'] = tk.NORMAL
@@ -163,25 +224,37 @@ def manual_start():
   global relay_open
   global manual_options
   global selected_manual_option
-  global my_canvas
+  global button_start
+  global button_manual
+  global button_exit
   global completion_text
   global selected_relay_text
+  global selected_relay
+  global local_start_time
+  global selected_run
 
   # set default state at the beginning of this function
-  my_canvas.delete(completion_text)
-  my_canvas.delete(selected_relay_text)
+  MY_CANVAS.delete(completion_text)
+  MY_CANVAS.delete(selected_relay_text)
   default_state()
 
   # get which relay was selected
   selected_relay = str(selected_manual_option.get()) 
 
+  # set start time and selected run
+  local_start_time = time.localtime()
+  selected_run = "Manual"
+
   # If off is selected, return to default state
   if selected_relay == "Off": 
-    my_canvas.delete(selected_relay_text)
+    MY_CANVAS.delete(selected_relay_text)
     default_state()
 
   # If relay selected isn't off, open that relay.
   else: 
+    button_start['state'] = tk.DISABLED 
+    button_manual['state'] = tk.DISABLED
+    button_exit['state'] = tk.DISABLED
     position = []
     for i in range(len(manual_options)):
       if manual_options[i] == selected_relay:
@@ -194,19 +267,17 @@ def manual_start():
 #================================= Widgets: ==================================#
 def window_inti():
   """ Initializes the GUI window and sets the GUI background."""
-  global my_canvas
-  global win
+  global background
 
   #Initialize the GUI window "win"
-  win.title("Relay Control GUI")
-  win.minsize(1280,660)
-  time.sleep(0.5)
+  WIN.title("Relay Control GUI")
+  WIN.minsize(1280,660)
+
+  # create canvas widget
+  MY_CANVAS.pack(fill = "both", expand=TRUE)
 
   # set up background image
-  background = tk.PhotoImage(file="./birchbg.png")
-  my_canvas = Canvas(win, width = 1280, height= 660)
-  my_canvas.pack(fill = "both", expand=TRUE)
-  my_canvas.create_image(0,0, image = background, anchor = 'nw')
+  MY_CANVAS.create_image(0, 0, image = background, anchor = 'nw')
 
 def setup_window():
   """ Sets up all the buttons and text within the GUI """
@@ -215,99 +286,99 @@ def setup_window():
   global selected_manual_option
   global button_start
   global button_manual
+  global button_exit
 
   #================= Left Side =================#
   #Manual Drop Down Menu:
   manual_options  = list(Relay_Names)
   manual_options[0] = "Off"
-  selected_manual_option = tk.StringVar(win)
+  selected_manual_option = tk.StringVar(WIN)
   selected_manual_option.set(manual_options[0]) # default value
 
   # Label Manual Select
-  my_canvas.create_text(50, 80, text="Manual Select", font=TITLE_FONT, 
+  MY_CANVAS.create_text(50, 80, text="Manual Select", font=TITLE_FONT, 
                         anchor = 'nw')
 
   # Menu Manual Select
-  manual_selection = OptionMenu(win, selected_manual_option, *manual_options)
-  button_manual_selection = my_canvas.create_window(120,160, anchor = 'nw', 
+  manual_selection = OptionMenu(WIN, selected_manual_option, *manual_options)
+  button_manual_selection = MY_CANVAS.create_window(120,160, anchor = 'nw', 
                                                     window=manual_selection)
   manual_selection.config(font=REGULAR_FONT, bg = 'white')
-  menu = win.nametowidget(manual_selection.menuname) 
+  menu = WIN.nametowidget(manual_selection.menuname) 
   menu.config(font=REGULAR_FONT, bg = 'white')
 
   # Label Automated Run
-  my_canvas.create_text(50, 300, text="Automated Run:", font=TITLE_FONT, 
+  MY_CANVAS.create_text(50, 300, text="Automated Run:", font=TITLE_FONT, 
                         anchor = 'nw')
 
   # Entry Relay Time
-  my_canvas.create_text(50,380, text="Sequence Time (sec):", font=REGULAR_FONT, 
+  MY_CANVAS.create_text(50,380, text="Sequence Time (sec):", font=REGULAR_FONT, 
                         anchor = 'nw')
-  relay_time = Entry(win, bd=6, width=3, font=INPUT_FONT)
-  relay_time_window = my_canvas.create_window(450,360, anchor = 'nw', 
+  relay_time = Entry(WIN, bd=6, width=3, font=INPUT_FONT)
+  relay_time_window = MY_CANVAS.create_window(450,360, anchor = 'nw', 
                                               window=relay_time)
 
   #================= Right Side ================#
   # Button Manual Start
-  button_manual = Button(win, text="Manual Start", font=TITLE_FONT, 
+  button_manual = Button(WIN, text="Manual Start", font=TITLE_FONT, 
                          command=manual_start)
-  button_manual_window = my_canvas.create_window(638,165, anchor = 'nw', 
+  button_manual_window = MY_CANVAS.create_window(638,165, anchor = 'nw', 
                                                  window=button_manual)
   
   # Button Automated Start
-  button_start = Button(win, text="Automated Start", font=TITLE_FONT, 
+  button_start = Button(WIN, text="Automated Start", font=TITLE_FONT, 
                         command=button_starter)
-  button_start_window = my_canvas.create_window(638,330, anchor = 'nw', 
+  button_start_window = MY_CANVAS.create_window(638,330, anchor = 'nw', 
                                                 window=button_start) 
   
   # Button Stop
   # bg = "#fff494"
-  button_stop = Button(win, text="STOP", font=TITLE_FONT, fg = "red", 
+  button_stop = Button(WIN, text="STOP", font=TITLE_FONT, fg = "red", 
                        relief = "solid", command = button_stop_command) 
-  button_stop_window = my_canvas.create_window(638,495, anchor = 'nw', 
+  button_stop_window = MY_CANVAS.create_window(638,495, anchor = 'nw', 
                                                window=button_stop)
   
   # Button End
-  button_end = Button(win, text="EXIT", font=TITLE_FONT, 
+  button_exit = Button(WIN, text="EXIT", font=TITLE_FONT, 
                       command = close_gui)
-  button_end_window = my_canvas.create_window(900, 495, anchor = 'nw',
-                                              window=button_end)
+  button_exit_window = MY_CANVAS.create_window(900, 495, anchor = 'nw',
+                                              window=button_exit)
 
 #============================ Updating Widgets: ==============================#
 def update():
   """ Updates GUI text that states which relay is currently open/being tested """
-  global win
   global selected_relay_text
   global relay_open
 
   # update text in GUI
-  my_canvas.delete(selected_relay_text)
-  selected_relay_text = my_canvas.create_text(50, 550, text="Relay selected is " + str(relay_open), 
+  MY_CANVAS.delete(selected_relay_text)
+  selected_relay_text = MY_CANVAS.create_text(50, 550, text="Relay selected is " + str(relay_open), 
                                               font=REGULAR_FONT, anchor = 'nw')
-  win.after(100, update)
+  WIN.after(100, update)
 
 def timeupdate():
   """ Updates the 'Run Percent Completed: ' text in the GUI based on how much 
   time has passed """
   global delay 
   global start_time
-  global my_canvas
   global completion_text
   global elapsed
   global button_start
+  global selected_run
 
   # calculate run progress
   if button_start['state']==tk.NORMAL:
     start_time = 0.0
-  elif button_start['state']==tk.DISABLED:
+  elif button_start['state']==tk.DISABLED and selected_run=="Automated":
     elapsed = (time.time() - start_time)*100/(16*delay)
 
   # update text in GUI
-  my_canvas.delete(completion_text)
-  completion_text = my_canvas.create_text(50, 495, 
+  MY_CANVAS.delete(completion_text)
+  completion_text = MY_CANVAS.create_text(50, 495, 
                             text="Run Percent Complete: " + f"{elapsed:.0f}" + "%", 
                             font=REGULAR_FONT, anchor = 'nw') 
 
-  win.after(100,timeupdate)
+  WIN.after(100,timeupdate)
 
 #================================= Close GUI =================================#
 def close_gui():
@@ -335,12 +406,17 @@ def main():
   window_inti()
   setup_window()
 
+  # create log file
+  with open(f'{DATE}.csv', 'a', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+    writer.writeheader()
+
   # update relay and run status
   update()
   timeupdate()
   
-  win.state('normal')
-  win.mainloop()
+  WIN.state('normal')
+  WIN.mainloop()
 
 if __name__ == '__main__':
   main()
